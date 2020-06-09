71c2dd7fcb0868117f5ba884befbb7e29bc2ed03
dinkiebinkie <b@binkie.town>
2020-06-08 21:08:29 -0400

    trying to find error

--- lib/groups/group_class.dart
+++ lib/groups/group_class.dart
@@ -1,16 +1,16 @@
 // data object for counter
-class GroupClass {
-  int id; // unique id
-  List counters; // list of counter ID's to put in
-  String title; // title of group
+//class GroupClass {
+//  int id; // unique id
+//  List counters; // list of counter ID's to put in
+//  String title; // title of group
 
   // String color; // color to make group
 
   // constructor
   // with curly brackets we need to name them
   // without you have to make sure is in order
-  CounterClass({this.count, this.title, this.id, this.selected, this.incrementAmount});
-}
+//  CounterClass({this.count, this.title, this.id, this.selected, this.incrementAmount});
+//}
 
 // to make a new counter do the following:
 // Counter( ${all properties} )
